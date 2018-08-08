a2f8d0b681438895cad4e8136939cc98d1309317
atthakorn <atthakorn@gmail.com>
2018-08-08 15:34:00 +0700

    init project structure

---
+++ cmd/fetch/main.go
@@ -0,0 +1,36 @@
+package main
+
+
+import (
+	"github.com/gocolly/colly"
+	"fmt"
+)
+
+func main()  {
+	// Instantiate default collector
+	c := colly.NewCollector(
+		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
+		colly.AllowedDomains("www.pttgcgroup.com"),
+	)
+
+
+	// On every a element which has href attribute call callback
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %s\n",link)
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	// Start scraping on https://hackerspaces.org
+	c.Visit("http://www.pttgcgroup.com/en/home")
+}
+
+
