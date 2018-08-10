bfc8ab2fb3351e80e7f674caccfae5c47d94ca7a
atthakorn <atthakorn@gmail.com>
2018-08-10 21:40:27 +0700

    wrap colly callback in Crawler struct method

--- internal/crawler/crawler.go
+++ internal/crawler/crawler.go
@@ -42,7 +42,6 @@ func (c *Crawler) isFileByExtension(link string) bool {
 	return false
 }
 
-
 // Start scraping
 func (c *Crawler) Start() {
 
@@ -59,6 +58,71 @@ func (c *Crawler) Start() {
 	fmt.Printf("Fetching Complete: %d pages in %s\n", c.total, elapsed)
 
 }
+
+
+func (c *Crawler) onHtml() colly.HTMLCallback {
+
+	return func(e *colly.HTMLElement) {
+
+		c.mux.Lock()
+		defer c.mux.Unlock()
+
+		contentType := e.Response.Headers.Get("Content-Type")
+
+		link := e.Attr("href")
+		//validate url
+		_, err := url.ParseRequestURI(link)
+
+		if err == nil && !(c.isFileByContentType(contentType) || c.isFileByExtension(link)) {
+
+			_, ok := c.crawledLinks[link]
+
+			if !ok {
+
+				c.crawledLinks[link] = link
+				// Visit link found on page, only those crawledLinks are visited which are in AllowedDomains
+				e.Request.Visit(e.Request.AbsoluteURL(link))
+			}
+		}
+
+	}
+}
+
+
+func (c *Crawler) onScraped() colly.ScrapedCallback {
+
+	return func(r *colly.Response) {
+		c.total++
+		fmt.Printf("Scraped: %s\n", r.Request.URL)
+	}
+}
+
+func (c *Crawler) onError() colly.ErrorCallback {
+
+	return func(r *colly.Response, e error) {
+
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", e)
+
+	}
+}
+
+func Make() *Crawler {
+
+	sites := viper.GetStringSlice("sites")
+	maxDepth := viper.GetInt("maxDepth")
+	parallelism := viper.GetInt("parallelism")
+	delay := viper.GetInt("delay")
+
+	crawler := &Crawler{
+		sites:       sites,
+		maxDepth:    maxDepth,
+		parallelism: parallelism,
+		delay:       delay,
+	}
+	crawler.init()
+	return crawler
+}
+
 
 
 // Crawer initializer
@@ -88,59 +152,14 @@ func (c *Crawler) init() {
 	collector.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: c.parallelism})
 
 	// Called after response received
-	collector.OnScraped(func(r *colly.Response) {
-		c.total++
-		fmt.Printf("Scraped: %s\n", r.Request.URL)
-	})
+	collector.OnScraped(c.onScraped())
 
 	// On every a element which has href attribute call callback
-	collector.OnHTML("a[href], area[href]", func(e *colly.HTMLElement) {
-
-		c.mux.Lock()
-		defer c.mux.Unlock()
-
-		contentType := e.Response.Headers.Get("Content-Type")
-
-		link := e.Attr("href")
-		//validate url
-		_, err := url.ParseRequestURI(link)
-
-		if err == nil && !(c.isFileByContentType(contentType) || c.isFileByExtension(link)) {
-
-			_, ok := c.crawledLinks[link]
-
-			if !ok {
-
-				c.crawledLinks[link] = link
-				// Visit link found on page, only those crawledLinks are visited which are in AllowedDomains
-				e.Request.Visit(e.Request.AbsoluteURL(link))
-			}
-		}
-
-	})
+	collector.OnHTML("a[href], area[href]", c.onHtml())
 
 	// Called if error occured during the request
-	collector.OnError(func(r *colly.Response, err error) {
-		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
-	})
+	collector.OnError(c.onError())
 
 	c.collector = collector
 
-}
-
-func Make() *Crawler {
-
-	sites := viper.GetStringSlice("sites")
-	maxDepth := viper.GetInt("maxDepth")
-	parallelism := viper.GetInt("parallelism")
-	delay := viper.GetInt("delay")
-
-	crawler := &Crawler{
-		sites:       sites,
-		maxDepth:    maxDepth,
-		parallelism: parallelism,
-		delay:       delay,
-	}
-	crawler.init()
-	return crawler
 }
